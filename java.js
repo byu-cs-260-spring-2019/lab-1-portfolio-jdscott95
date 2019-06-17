a238ab0231b1615aa803d5aeabbe9184ca0e6861
jdscott95 <46513091+jdscott95@users.noreply.github.com>
2019-06-16 18:28:39 -0600

    Add files via upload
    this is the redo of lab 1 to get all criteria

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
 var jobs = [
-  'Hello World!',
-  'String Parser',
-  'Relational Database',
-  '3-D Maze'
+  'Don\'t hate what you don\'t understand. - John Lennon',
+  'The good life is inspired by love and guided by knowledge. - Bertrand Russell',
+  'It is not only necessary to love, it is necessary to say so. - French proverb',
+  'Reason and love are sworn enemies. - Pierre Corneille'
 ]
 
-function newProject()
+function newQuote()
 {
   var randomNumber = Math.floor(Math.random() * (jobs.length));
-  document.getElementById('projectName').innerHTML = jobs[randomNumber];
+  document.getElementById('quote').innerHTML = jobs[randomNumber];
 }
